ef9319b48e0899e30fbafbd75ce5d9ef5b14cfb7
smbdawsm <nikolay.nikishev@icloud.com>
2020-10-13 00:05:24 +0700

    убран config.py

--- GsheetParser.py
+++ GsheetParser.py
@@ -4,8 +4,13 @@ import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 import itertools
 from mongo import insert_document, objects_collection
-import config
-
+CREDENTIALS_FILE = 'objectspy-16fdec13bb13.json'  # Имя файла с закрытым ключом
+# Читаем ключи из файла
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в системе
+service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API 
+spreadsheetId = "1Bj2iwLI4rdxtG-m14cpTFtQ89_sI5_CJHiQRDXRspgg"
+print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
 spreadsheet = service.spreadsheets().get(spreadsheetId = spreadsheetId).execute()
 def ListsOfSheet(spreadsheet):
     return sheetList = spreadsheet.get('sheets')
