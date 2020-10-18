5a498fdc86cc50e4b8de94633fbff3a1fc440dc0
smbdawsm <nikolay.nikishev@icloud.com>
2020-10-18 12:50:05 +0700

    CIDR fixed

--- api.py
+++ api.py
@@ -1,13 +1,28 @@
+import ast
 from fastapi import FastAPI
 from fastapi import HTTPException
 from mongo import Database
 from typing import Optional
-import ast
 from bson.objectid import ObjectId
 from properties import OPTIONS, NAME
+from fastapi.middleware.cors import CORSMiddleware
+
 
 database = Database()
 app = FastAPI(title=NAME)
+
+origins = [
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/objects")
 def read_root():
